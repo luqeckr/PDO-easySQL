0fca88dee367ee5b83756eccb989b89d966c5409
Luqman Hakim <luqeckr@gmail.com>
2016-03-19 10:23:12 +0800

    add WHERE function

--- pdo_crud.php
+++ pdo_crud.php
@@ -17,13 +17,13 @@ class Database {
 	private $db_user = "username"; // Change as required
 	private $db_pass = "password";  // Change as required
 	private $db_name = "database"; // Change as required
-	
-	/*
-	 * Extra variables that are required by other function such as boolean con variable
-	 */
-	private $con = false; // Check to see if the connection is active
+    
+    /*
+     * Extra variables that are required by other function such as boolean con variable
+     */
+    private $con = false; // Check to see if the connection is active
     protected static $myconn = ""; // This will be our mysqli object
-	public $result = array(); // Any results from a query will be stored here
+    public $result = array(); // Any results from a query will be stored here
     public $myQuery = "";// used for debugging process with SQL return
     private $numResults = "";// used for returning the number of rows
     private $sel_table = ""; // select query 
@@ -35,8 +35,9 @@ class Database {
 
     }
 
-	// Function to make connection to database
-	public function connect(){
+    
+    // Function to make connection to database
+    public function connect(){
         try {
             if(!$this->con) {
                 $dsn = "mysql:host={$this->db_host}";
@@ -54,14 +55,14 @@ class Database {
             return false;
         }
 
-	}
+    }
 
-	
-	// Function to disconnect from the database
+    
+    // Function to disconnect from the database
     public function disconnect(){
-    	// If there is a connection to the database
-    	if($this->con){
-    		// We have found a connection, try to close it
+        // If there is a connection to the database
+        if($this->con){
+            // We have found a connection, try to close it
             $this->myconn = null;
             if ($this->myconn) {
                 return false; // connection still not closed
@@ -70,50 +71,50 @@ class Database {
                 return true; // connection is closed
             }
 
-		}
+        }
     }
-	
-	public function sql($sql){
-		$query = $this->myconn->query($sql);
+    
+    public function sql($sql){
+        $query = $this->myconn->query($sql);
         $this->myQuery = $sql; // Pass back the SQL
-		if($query){
-			// If the query returns >= 1 assign the number of rows to numResults
-			$this->numResults = $this->last_row_count();
-			// Loop through the query results by the number of rows returned
-			for($i = 0; $i < $this->numResults; $i++){
-				$r = $query->fetch(PDO::FETCH_ASSOC);
-               	$key = array_keys($r);
-               	for($x = 0; $x < count($key); $x++){
-               		// Sanitizes keys so only alphavalues are allowed
-                   	if(!is_int($key[$x])){
-                   		if($this->last_row_count() >= 1){
-                   			$this->result[$i][$key[$x]] = $r[$key[$x]];
-						}else{
-							$this->result = null;
-						}
-					}
-				}
-			}
-			return true; // Query was successful
-		}else{
-			array_push($this->result,$this->myconn->error);
-			return false; // No rows where returned
-		}
-	}
-	
-	// Function to SELECT from the database
-	public function select($table, $rows = '*', $join = null, $where = null, $order = null, $limit = null){
-		// Create query from the variables passed to the function
-		$q = 'SELECT '.$rows.' FROM '.$table;
-		if($join != null){
-			$q .= ' '.$join; //$q .= ' JOIN '.$join; # for ability to use LEFT/RIGHT/INNER/OUTER JOIN
-		}
+        if($query){
+            // If the query returns >= 1 assign the number of rows to numResults
+            $this->numResults = $this->last_row_count();
+            // Loop through the query results by the number of rows returned
+            for($i = 0; $i < $this->numResults; $i++){
+                $r = $query->fetch(PDO::FETCH_ASSOC);
+                $key = array_keys($r);
+                for($x = 0; $x < count($key); $x++){
+                    // Sanitizes keys so only alphavalues are allowed
+                    if(!is_int($key[$x])){
+                        if($this->last_row_count() >= 1){
+                            $this->result[$i][$key[$x]] = $r[$key[$x]];
+                        }else{
+                            $this->result = null;
+                        }
+                    }
+                }
+            }
+            return true; // Query was successful
+        }else{
+            array_push($this->result,$this->myconn->error);
+            return false; // No rows where returned
+        }
+    }
+    
+    // Function to SELECT from the database
+    public function select($table, $rows = '*', $join = null, $where = null, $order = null, $limit = null){
+        // Create query from the variables passed to the function
+        $q = 'SELECT '.$rows.' FROM '.$table;
+        if($join != null){
+            $q .= ' '.$join; //$q .= ' JOIN '.$join; # for ability to use LEFT/RIGHT/INNER/OUTER JOIN
+        }
         if($where != null){
-        	$q .= ' WHERE '.$where;
-		}
+            $q .= ' WHERE '.$where;
+        }
         if($order != null){
             $q .= ' ORDER BY '.$order;
-		}
+        }
         if($limit != null){
             $q .= ' LIMIT '.$limit;
         }
@@ -121,58 +122,57 @@ class Database {
         // echo $table;
         $this->myQuery = $q; // Pass back the SQL
         //echo $q;
-		// Check to see if the table exists
+        // Check to see if the table exists
         if($this->tableExists($table)){
-        	// The table exists, run the query
-        	$query = $this->myconn->query($q);    
-			if($query){
-				// If the query returns >= 1 assign the number of rows to numResults
-				$this->numResults = $this->last_row_count();
-				// Loop through the query results by the number of rows returned
-				for($i = 0; $i < $this->numResults; $i++){
-					$r = $query->fetch(PDO::FETCH_ASSOC);
-                	$key = array_keys($r);
-                	for($x = 0; $x < count($key); $x++){
-                		// Sanitizes keys so only alphavalues are allowed
-                    	if(!is_int($key[$x])){
-                    		if($this->last_row_count() >= 1){
-                    			$this->result[$i][$key[$x]] = $r[$key[$x]];
-							}else{
-								$this->result[$i][$key[$x]] = null;
-							}
-						}
-					}
-				}
-				return true; // Query was successful
-			}else{
+            // The table exists, run the query
+            $query = $this->myconn->query($q);    
+            if($query){
+                // If the query returns >= 1 assign the number of rows to numResults
+                $this->numResults = $this->last_row_count();
+                // Loop through the query results by the number of rows returned
+                for($i = 0; $i < $this->numResults; $i++){
+                    $r = $query->fetch(PDO::FETCH_ASSOC);
+                    $key = array_keys($r);
+                    for($x = 0; $x < count($key); $x++){
+                        // Sanitizes keys so only alphavalues are allowed
+                        if(!is_int($key[$x])){
+                            if($this->last_row_count() >= 1){
+                                $this->result[$i][$key[$x]] = $r[$key[$x]];
+                            }else{
+                                $this->result[$i][$key[$x]] = null;
+                            }
+                        }
+                    }
+                }
+                return true; // Query was successful
+            }else{
                 array_push($this->result,$this->myconn->errorInfo());
-				return false; // No rows where returned
-			}
-      	}else{
-      		return false; // Table does not exist
-    	}
+                return false; // No rows where returned
+            }
+        }else{
+            return false; // Table does not exist
+        }
     }
-	
-	// Function to insert into the database
+    
+    // Function to insert into the database
     public function insert($table,$params=array()){
-    	// Check to see if the table exists
-    	 if($this->tableExists($table)){
-    	 	$sql='INSERT INTO `'.$table.'` (`'.implode('`, `',array_keys($params)).'`) VALUES ("' . implode('", "', $params) . '")';
+        // Check to see if the table exists
+         if($this->tableExists($table)){
+            $sql='INSERT INTO `'.$table.'` (`'.implode('`, `',array_keys($params)).'`) VALUES ("' . implode('", "', $params) . '")';
             $this->myQuery = $sql; // Pass back the SQL
             // Make the query to insert to the database
             if($ins = $this->myconn->query($sql)){
-            	array_push($this->result,$this->myconn->lastInsertId());
+                array_push($this->result,$this->myconn->lastInsertId());
                 return true; // The data has been inserted
             }else{
-            	array_push($this->result,$this->myconn->errorInfo());
+                array_push($this->result,$this->myconn->errorInfo());
                 return false; // The data has not been inserted
             }
         }else{
-        	return false; // Table does not exist
+            return false; // Table does not exist
         }
     }
 
-    // Function to insert into the database, using prepared statement
     public function insert_prep($table, $params=array()) {
         // Check to see if the table exists
         if($this->tableExists($table)) {
@@ -189,9 +189,15 @@ class Database {
             echo $sql;
             /*** prepare and execute ***/
             $stmt = $this->myconn->prepare($sql);
-
-            if ($stmt->execute($params)) {
+            foreach ($params as $key => $value) {
+                $param1 = ':'.$key;
+                $stmt->bindValue($param1, $value);
+            }
+            
+            if ($stmt->execute()) {
+            //if ($stmt->execute($params)) {
                 array_push($this->result,$this->myconn->lastInsertId());
+
                 return true; // The data has been inserted
             } else{
                 array_push($this->result,$this->myconn->errorInfo());
@@ -202,13 +208,13 @@ class Database {
         }
         
     }
-	
-	//Function to delete table or row(s) from database
+    
+    //Function to delete table or row(s) from database
     public function delete($table,$where = null){
-    	// Check to see if table exists
-    	 if($this->tableExists($table)){
-    	 	// The table exists check to see if we are deleting rows or table
-    	 	if($where == null){
+        // Check to see if table exists
+         if($this->tableExists($table)){
+            // The table exists check to see if we are deleting rows or table
+            if($where == null){
                 $delete = 'DROP TABLE '.$table; // Create query to delete table
             }else{
                 $delete = 'DELETE FROM '.$table.' WHERE '.$where; // Create query to delete rows
@@ -216,56 +222,56 @@ class Database {
             // Submit query to database
             $del = $this->myconn->prepare($delete);
             if($del->execute()){
-            	array_push($this->result,$del->rowCount());
+                array_push($this->result,$del->rowCount());
                 $this->myQuery = $delete; // Pass back the SQL
                 return true; // The query exectued correctly 
             }else{
-            	array_push($this->result,$this->myconn->errorInfo());
-               	return false; // The query did not execute correctly
+                array_push($this->result,$this->myconn->errorInfo());
+                return false; // The query did not execute correctly
             }
         }else{
             return false; // The table does not exist
         }
     }
-	
-	// Function to update row in database
+    
+    // Function to update row in database
     public function update($table,$params=array(),$where){
-    	// Check to see if table exists
-    	if($this->tableExists($table)){
+        // Check to see if table exists
+        if($this->tableExists($table)){
             $fieldnames = array_keys($params);
-    		// Create Array to hold all the columns to update
+            // Create Array to hold all the columns to update
             $args=array();
             foreach ($fieldnames as $field) {
                 $args[]=$field.'=:'.$field;
             }
-			
-			// Create the query
-			$sql='UPDATE '.$table.' SET '.implode(',',$args).' WHERE '.$where;
-			// Make query to database
+            
+            // Create the query
+            $sql='UPDATE '.$table.' SET '.implode(',',$args).' WHERE '.$where;
+            // Make query to database
             $this->myQuery = $sql; // Pass back the SQL
             $stmt = $this->myconn->prepare($sql);
             if($stmt->execute($params)){
-            	array_push($this->result,$stmt->rowCount());
-            	return true; // Update has been successful
+                array_push($this->result,$stmt->rowCount());
+                return true; // Update has been successful
             }else{
-            	array_push($this->result,$this->myconn->errorInfo());
+                array_push($this->result,$this->myconn->errorInfo());
                 return false; // Update has not been successful
             }
         }else{
             return false; // The table does not exist
         }
     }
-	
-	// Private function to check if table exists for use with queries
-	private function tableExists($table){
+    
+    // Private function to check if table exists for use with queries
+    private function tableExists($table){
         $table = explode(' ', $table);
         $table = $table[0];
-		$tablesInDb = $this->myconn->query('SHOW TABLES FROM '.$this->db_name.' LIKE "'.$table.'"');
+        $tablesInDb = $this->myconn->query('SHOW TABLES FROM '.$this->db_name.' LIKE "'.$table.'"');
         if($tablesInDb){
-        	if($this->last_row_count() == 1){
+            if($this->last_row_count() == 1){
                 return true; // The table exists
             }else{
-            	array_push($this->result,$table." does not exist in this database");
+                array_push($this->result,$table." does not exist in this database");
                 return false; // The table does not exist
             }
         }
@@ -274,8 +280,8 @@ class Database {
     public function last_row_count() {
         return $this->myconn->query("SELECT FOUND_ROWS()")->fetchColumn();
     }
-	
-	// Public function to return the data to the user
+    
+    // Public function to return the data to the user
     public function getResult(){
         $val = $this->result;
         $this->result = array();
@@ -297,16 +303,18 @@ class Database {
     }
 
     // Escape your string
-    public function escapeString($data){
+    public function escapeIt($data){
         return $this->myconn->real_escape_string($data);
     }
+
+    
+
 
 }
 
 
 class Builder extends Database {
 
- 
     protected static $myconn;
 
     public function __construct(Database $db) {
@@ -319,6 +327,8 @@ class Builder extends Database {
         $this->qBuild = ''; // reset value
         $this->qBuild = new stdClass;
         $this->qBuild->where = array();
+        $this->qBuild->whereValue = array();
+        $this->qBuild->whereCol = array();
         $this->qBuild->join = array();
         $this->qBuild->insertdata = array();
     }
@@ -367,27 +377,29 @@ class Builder extends Database {
        
     }
 
+    // build update table query
     public function update_to($table) {
         $fieldnames = array_keys($this->qBuild->insertdata);
-        $sql = "UPDATE $table SET";
+        $sql = "UPDATE $table SET ";
         /*** set the field names ***/
         $fields = implode(', ', $fieldnames);
         /*** set the placeholders ***/
         $bound = ':' . implode(', :', $fieldnames);
         /*** put the query together ***/
 
-        $i=0;
+        $i=0; $setdata = '';
         foreach ($fieldnames as $key) {
-            if ($i > 0) { $prepend = ', ' } else { $prepend = ''; }
-            $setdata = $prepend . $key.' = :'.$key;
+            if ($i > 0) { $prepend = ', '; } else { $prepend = ''; }
+            $setdata.= $prepend . $key.' = :'.$key;
             $i++;
         }
 
         $sql .= $setdata;
+        
         if (!empty($this->qBuild->where)) {
             $where = implode(' ', $this->qBuild->where);
             $sql.= ' WHERE '.$where;
-        }
+        } 
 
         $this->myQuery = $sql;
         /*** prepare and execute ***/
@@ -396,10 +408,20 @@ class Builder extends Database {
             $param1 = ':'.$key;
             $stmt->bindValue($param1, $value);
         }
-        
+
+        // bind where value
+        $i=0;
+        foreach ($this->qBuild->whereCol as $key) {
+            $param1 = ':w'.$key;
+            $stmt->bindValue($param1, $this->qBuild->whereValue[$i]);
+            array_push($this->result, $param1.' '.$this->qBuild->whereValue[$i]);
+            $i++;
+        }
+
+        // execute
         if ($stmt->execute()) {
         //if ($stmt->execute($params)) {
-            array_push($this->result,$this->myconn->lastInsertId());
+            array_push($this->result,$stmt->rowCount());
             return true; // The data has been inserted
         } else{
             array_push($this->result,$this->myconn->errorInfo());
@@ -415,9 +437,19 @@ class Builder extends Database {
         array_push($this->qBuild->join, $data);
     }
 
-    public function where($data, $op=null) {
-        $data = $op ? ' '.$op.' '.$data : $data;
+    public function where($column, $value, $opt=null) {
+        // check for operator in $column
+        $col = explode(' ', $column);
+        $oper = isset($col[1]) ? $col[1] : '=';
+        
+        $wheredata = $col[0].$oper.':w'.$col[0]; // column =>!=< :wcolumn
+
+        // is it more than one WHERE?
+        $data = $opt ? ' '.$opt.' '.$wheredata : $wheredata;
+        array_push($this->qBuild->whereCol, $col[0]);
         array_push($this->qBuild->where, $data);
+        array_push($this->qBuild->whereValue, $value);
+        array_push($this->result, $data.' xx '.$value);
     }
 
 
